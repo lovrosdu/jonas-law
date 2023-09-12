0a64deab6d375beeefda92776b8fbbd24d3219bf
lovrosdu <lugovic@imada.sdu.dk>
2023-09-12 12:19:58 +0200

    Rename explanation to text

--- tree-sitter-jonas-law/jonas-law-explanations.js
+++ tree-sitter-jonas-law/jonas-law-explanations.js
@@ -3,7 +3,7 @@ const jonas_law = require('./jonas-law');
 module.exports = grammar(jonas_law, {
     name: 'jonas_law_explanations',
     rules: {
-        clause: $ => seq($.compound, ';', $.explanation),
-        explanation: $ => /.*/
+        clause: $ => seq($.compound, ';', $.text),
+        text: $ => /.*/
     }
 });
